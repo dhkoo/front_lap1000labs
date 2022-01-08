f797bd4201afb2335fa5ee0011f09d44d93a0015
dhkoo <amainlog@gmail.com>
2022-01-09 00:41:39 +0900

    feat: basic main page

---
+++ src/constants/images.ts
@@ -0,0 +1,3 @@
+const imageLocation = window.location.origin;
+
+export const Lap1000Logo = imageLocation.concat('/images/lap1000_logo.png');
---
+++ src/views/Main/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  height: 100vh;
+  background-color: black;
+`;
+
+export const ContentText = styled.div`
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  color: white;
+`;
+
+export const Lap1000Logo = styled.img`
+  width: 150px;
+  border: solid white;
+`;
