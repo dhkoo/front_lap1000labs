92709012c979f6b19ec066d118d6bc69fbd9c919
dhkoo <amainlog@gmail.com>
2022-02-14 21:40:29 +0900

    fix: fix donation viewing

--- src/contracts/addrBook.ts
+++ src/contracts/addrBook.ts
@@ -2,10 +2,10 @@ export const contractAddr: { [key: string]: string } = {
   pala: '0x7a1cdca99fe5995ab8e317ede8495c07cbf488ad',
   AlapViewer: '0x2bE97CE60051A16C477008fdFb15A0b10d5C461b',
   Donation: '0xF1F85169070B81d7035933286c3C21411c94448B',
-  DonationViewer: '0xE38c5C92b59bD3B394cBb52b46Cf50168A9E5127',
+  DonationViewer: '0x071cE5071Ce53284ED021aD472b4Bc8CcC7be1d1',
 };
 
 export const gateway: { [key: string]: string } = {
-  cypress: 'https://public-node-api.klaytnapi.com/v1/cypress',
+  cypress: 'https://klaytn-en.sixnetwork.io:8651/',
   baobab: 'https://public-node-api.klaytnapi.com/v1/baobab',
 };
