4b3ef2e7bd705b622580b9e3b44d380d9f7c48a5
dhkoo <amainlog@gmail.com>
2022-06-15 00:03:09 +0900

    fix: remove margin in lap1000logo

--- src/components/TopBar/style.ts
+++ src/components/TopBar/style.ts
@@ -47,11 +47,6 @@ export const LogoButton = styled(NavLink)`
 export const Lap1000Logo = styled.img`
   box-sizing: border-box;
   height: 50px;
-  margin-right: 10px;
-
-  @media screen and (max-width: 370px) {
-    height: 40px;
-  }
 `;
 
 // Profile
